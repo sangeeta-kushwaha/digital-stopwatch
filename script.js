2d5778dd40c51a188a5e5bff2ad09b02aee7b193
sangeeta-kushwaha <sangeetakushwaha1205@gmail.com>
2025-03-26 23:40:43 +0530

    chnages done

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const setTimer = document.getElementById("displaytime")
 const start = document.getElementById("startBtn")
 const pause = document.getElementById("pauseBtn")
 const reSet = document.getElementById("resetBtn");
+const getStopedTime = document.getElementById("getTime")
+const datalists = document.getElementById("laplist");
+
+const clearList = document.getElementById("clearlist");
+
+
 
 // const spanid1 = document.getElementById("span1")
 // const spanid2 = document.getElementById("span2")
@@ -10,8 +16,9 @@ const reSet = document.getElementById("resetBtn");
 
 start.addEventListener("click", starts);
 pause.addEventListener("click",paused);
-reSet.addEventListener("click",reset)
-
+reSet.addEventListener("click",reset);
+getStopedTime.addEventListener("click",showstoptime);
+clearList.addEventListener("click", clearLists)
 
 
 let count =0;
@@ -58,3 +65,14 @@ function reset(){
 
 
 
+function showstoptime(){
+    const para = document.createElement("p");
+    para.innerText = `the stop time is ${count} sec`;
+    datalists.append(para)
+    console.log(para)
+}
+
+function clearLists(){
+    datalists.innerHTML = null;
+    console.log("clear")
+}
